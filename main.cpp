9363d52acecb55d3eb2ed8cd82b532ae0c64a4f9
Waveyyyy <waves-sec@protonmail.com>
2022-03-08 16:05:09 +0000

    Fix issue of user supplied resolution being overwrote by default
    Scope of the non default constructor that changes the resolution
    meant that it was deleted before it could be used, causing the
    default constructor values to be used instead.

--- main.cpp
+++ main.cpp
@@ -9,46 +9,26 @@
  * Takes an input from a user 
  * and ensures it is of type integer
  */
-int input_int(const char *msg)
+int input_int(const char *msg, int dfault)
 {
-    bool valid = {0};
     unsigned int_value = {};
     std::string input;
 
-    do
-    {
-        std::printf("%s", msg);
-        std::getline(std::cin, input);
-        std::stringstream usrInput(input);
-        if(usrInput >> int_value)
-            valid = 1;
-    } while (!valid);
+    std::printf("%s", msg);
+    std::getline(std::cin, input);
+    std::stringstream usrInput(input);
+    if(usrInput >> int_value)
+        return int_value;
 
-    return int_value;
+    return dfault;
 }
 
 
 int main()
 {
-    std::printf("Change resolution (Default 1920x1080)? [y/N] ");
-    char ans = tolower(std::getchar());
-    if (!(ans == '\n'))
-        std::getchar();
-    // default constructor
-    mandelbrot mb; 
-
-    // if user chooses custom resolution
-    // take input for width/height
-    // create object with supplied width/height
-    if(ans == 'y')
-    {
-        int width = {0};
-        int height = {0};
-
-        width = input_int("Enter a width (Default 1920) : ");
-        height = input_int("Enter a height (Default 1080) : ");
-        mandelbrot mb(width, height);
-    }
+    int width = input_int("Enter a width (Default 1920) : ", 1920);
+    int height = input_int("Enter a height (Default 1080) : ", 1080);
+    mandelbrot mb = mandelbrot(width, height);
 
     // run create mandelbrot function
     mb.create_mandelbrot();
